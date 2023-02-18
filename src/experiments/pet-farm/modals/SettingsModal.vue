8a8fd799fa6a2d448e1005521810aa234323959f
Chase Drift Ubuntu <chadonsom@gmail.com>
2023-02-18 08:33:52 -0500

    boids first draft (focus on only one otherPet)
    - they still need away to focus on the giant group too though so they don't
      all focus on one in front and go to the same place

--- src/experiments/pet-farm/modals/SettingsModal.vue
+++ src/experiments/pet-farm/modals/SettingsModal.vue
@@ -1,7 +1,11 @@
 <template>
   <Modal @close="modals.close(id)">
     <div class="flex flex-col">
-      <Button @click="reset()"><template #leading-icon>replay</template>Reset</Button>
+      <p class="my-3">Debug</p>
+      <MdcSwitch v-model="settings.debugDirection">Direction</MdcSwitch>
+      <MdcSwitch v-model="settings.debugFrontFeelers">Front feelers</MdcSwitch>
+      <MdcSwitch v-model="settings.debugBackFeelers">Back feelers</MdcSwitch>
+      <MdcSwitch v-model="settings.debugTooCloseTooFar">Too close / too far</MdcSwitch>
       <!-- <p class="my-3 italic">Not implemented yet</p> -->
       <!-- <MdcSwitch disabled class="text-gray-500 italic">Click mode</MdcSwitch> -->
     </div>
@@ -16,7 +20,7 @@ import Button from '@/core/buttons/Button.vue'
 import OutlinedTextfield from '@/core/fields/OutlinedTextfield.vue'
 import OutlinedSelect from '@/core/selects/OutlinedSelect.vue'
 import SelectOption from '@/core/selects/SelectOption.vue'
-import { reset } from '@/experiments/pet-farm'
+import { reset, settings } from '@/experiments/pet-farm'
 
 const props = defineProps({
   id: {
